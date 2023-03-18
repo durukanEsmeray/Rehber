4598c70b0326fa62559a6ddd2b89f8c56b77e5b5
durukanEsmeray <durukanesmeray@gmail.com>
2023-03-18 23:26:55 +0300

    Database katmanında yazdığım metotları Business katmanında kontrol altına aldım ve yapılması gereken işleri verdim.

--- DatabaseLogicLayer/DLL.cs
+++ DatabaseLogicLayer/DLL.cs
@@ -30,6 +30,26 @@ namespace DatabaseLogicLayer
                 con.Close();
         }
 
+        public int SistemKontrol(Kullanici K)
+        {
+            try
+            {
+                cmd = new SqlCommand("Select count(*) from Kullanici Where KullaniciAdi = @KullaniciAdi and Sifre = @Sifre", con);
+                BaglantiAyarla();
+                ReturnValues = (int)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                BaglantiAyarla();
+            }
+            return ReturnValues;
+        }
+
         public int KayitEkle(Rehber R)
         {
             try
@@ -63,6 +83,85 @@ namespace DatabaseLogicLayer
                 BaglantiAyarla();
             }
             return ReturnValues;
+        }
+
+        public int KayitDuzenle(Rehber R)
+        {
+            try
+            {
+                cmd = new SqlCommand(@"update Rehber
+Isim = @Isim,
+Soyisim = @Soyisim,
+TelefonNumarasiI = @TelefonNumarasiI,
+TelefonNumarasiII = @TelefonNumarasiII,
+TelefonNumarasiIII = @TelefonNumarasiIII,
+EmailAdres = @EmalAdres,
+Adres = @Adres,
+Aciklama = @Aciklama,
+Where
+ID = @ID
+", con);
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = R.ID;
+                cmd.Parameters.Add("@Isim", SqlDbType.NVarChar).Value = R.Isim;
+                cmd.Parameters.Add("@Soyisim", SqlDbType.NVarChar).Value = R.Soyisim;
+                cmd.Parameters.Add("@TelefonNumarasiI", SqlDbType.NVarChar).Value = R.TelefonNumarasiI;
+                cmd.Parameters.Add("@TelefonNumarasiII", SqlDbType.NVarChar).Value = R.TelefonNumarasiII;
+                cmd.Parameters.Add("@TelefonNumarasiIII", SqlDbType.NVarChar).Value = R.TelefonNumarasiIII;
+                cmd.Parameters.Add("@EmailAdres", SqlDbType.NVarChar).Value = R.EmailAdres;
+                cmd.Parameters.Add("@WebAdres", SqlDbType.NVarChar).Value = R.WebAdres;
+                cmd.Parameters.Add("@Adres", SqlDbType.NVarChar).Value = R.Adres;
+                cmd.Parameters.Add("@Aciklama", SqlDbType.NVarChar).Value = R.Aciklama;
+                BaglantiAyarla(); // C# ile sql arasındaki bağlantıyı açıyorum.
+                ReturnValues = cmd.ExecuteNonQuery(); // Hazırlamış olduğum insert sorgusunu sql server a göndereceğim.
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally 
+            {
+                BaglantiAyarla();
+            }
+            return ReturnValues;
+        }
+
+        public int KayitSil(Guid ID)
+        {
+            try
+            {
+                cmd = new SqlCommand(@"delete Rehber
+Where
+ID = @ID
+", con);
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = ID;
+                BaglantiAyarla(); // C# ile sql arasındaki bağlantıyı açıyorum.
+                ReturnValues = cmd.ExecuteNonQuery(); // Hazırlamış olduğum insert sorgusunu sql server a göndereceğim.
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            finally
+            {
+                BaglantiAyarla();
+            }
+            return ReturnValues;
+        }
+
+        public SqlDataReader KayitListe()
+        {
+            cmd = new SqlCommand("Select * from Rehber", con);
+            BaglantiAyarla();
+            return cmd.ExecuteReader();
+        }
+
+        public SqlDataReader KayitListeID(Guid ID)
+        {
+            cmd = new SqlCommand("Select * from Rehber Where ID = @ID", con);
+            cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = ID;
+            BaglantiAyarla();
+            return cmd.ExecuteReader();
         }
     }
 }
